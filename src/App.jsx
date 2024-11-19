637341cef565332d492d1d8e840b513a7c4ab148
RenatKramarovskyi <renatkram1352@gmail.com>
2024-11-19 10:18:41 +0200

    feat: added router skeleton in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
+import {BrowserRouter as Router, Routes, Route, Outlet} from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import AboutPage from "./pages/AboutPage/AboutPage.jsx";
+import ShopPage from "./pages/ShopPage/ShopPage.jsx";
+import Layout from "./comoponents/Layout/Layout.jsx";
 
 function App() {
 
   return (
-   <div>
-        It initialized now
-   </div>
+    <Router>
+        <Routes>
+            <Route path="/workspace" element={<Layout/>}>
+                <Route path="home" element={<HomePage/>} />
+                <Route path="about" element={<AboutPage/>} />
+                <Route path="shop" element={<ShopPage/>} />
+            </Route>
+        </Routes>
+    </Router>
   )
 }
 
